cf0292ebd4a077fd9f00ed689636008806e34409
Hamad <AlmarzooqiH23@gmail.com>
2024-09-24 14:18:12 +0400

    Fixed the naming since it was conflicting with termios-c_flag.h

--- commands.h
+++ commands.h
@@ -6,7 +6,7 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 12:24:25 by hamad             #+#    #+#             */
-/*   Updated: 2024/09/24 12:30:51 by hamad            ###   ########.fr       */
+/*   Updated: 2024/09/24 13:14:21 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,13 @@
 		   \ \____/ \ \_____\ \_\\ \_\ \_\\ \_\ \_\ \_\ \_\ \_\ \____/\ `\____\
 		    \/___/   \/_____/\/_/ \/_/\/_/ \/_/\/_/\/_/\/_/\/_/\/___/  \/_____/
 */
-# define ECHO "echo\0"
-# define CD "cd\0"
-# define PWD "pwd\0"
-# define EXPORT "export\0"
-# define UNSET "unset\0"
-# define ENV "env\0"
-# define exit "exit\0"
+# define ECHO_COMMAND "echo\0"
+# define CD_COMMAND "cd\0"
+# define PWD_COMMAND "pwd\0"
+# define EXPORT_COMMAND "export\0"
+# define UNSET_COMMAND "unset\0"
+# define ENV_COMMAND "env\0"
+# define EXIT_COMMAND "exit\0"
 /*
 				 ____    __       ______  ____    ____       
 				/\  _`\ /\ \     /\  _  \/\  _`\ /\  _`\     
@@ -37,5 +37,5 @@
 				   \ \_\   \ \____/ \ \_\ \_\ \____/\ `\____\
 				    \/_/    \/___/   \/_/\/_/\/___/  \/_____/
 */
-# define DASH_N "-n\0"
+# define DASH_N_FLAG "-n\0"
 #endif
