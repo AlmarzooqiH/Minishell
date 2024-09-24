4f0f7e71071b798e24b4000393138133476936ef
Hamad <AlmarzooqiH23@gmail.com>
2024-09-24 14:18:42 +0400

    Changed the naming only.

--- error.h
+++ error.h
@@ -6,11 +6,11 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 17:06:14 by hamad             #+#    #+#             */
-/*   Updated: 2024/09/23 17:07:02 by hamad            ###   ########.fr       */
+/*   Updated: 2024/09/24 13:20:25 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERROR_H
 # define ERROR_H
-# define WRONG_INPUT "Wrong input\0"
+# define INPUT_ERROR "Wrong input\0"
 #endif
