27fba12cec8bfbe276f358269c8c72777c4d44f7
Hamad <AlmarzooqiH23@gmail.com>
2024-09-30 21:56:37 +0400

    This file will hold some utilites that will help us with this project, currently it has the has_pipe function that will check if the input has some pipe(|)

---
+++ utils.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 21:26:39 by hamad             #+#    #+#             */
+/*   Updated: 2024/09/30 21:33:55 by hamad            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "includes/minishell.h"
+/*
+	@brief	This function will check if the passed commands contain a pipe('|')
+	@param	commands	This will hold all of the commands passed by the shell.
+	@param	len			This holds the length of the commands.
+*/
+int	has_pipe(char **commands, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!commands || !(*commands))
+		return (0);
+	while (i < len)
+	{
+		j = ft_skipspace(commands[i]);
+		while (commands[i][j])
+		{
+			if (commands[i][j] == '|')
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
