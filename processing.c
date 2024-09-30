11aeb6d76eb8ce2f7c66d65f9b7673c06ab10a6a
Hamad <AlmarzooqiH23@gmail.com>
2024-09-30 21:57:34 +0400

    Added an if statement that will check if the input has a pipe or not to process it diffrently.

--- processing.c
+++ processing.c
@@ -6,33 +6,37 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 13:03:10 by hamad             #+#    #+#             */
-/*   Updated: 2024/09/24 19:27:37 by hamad            ###   ########.fr       */
+/*   Updated: 2024/09/30 21:46:49 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
+void	process_commands(char **commands, size_t len)
+{
+	if (ft_strcmp(commands[0], ECHO_COMMAND))
+		process_echo(commands, len);
+}
+void	process_commands_wp(char **commands, size_t len)
+{
+	(void)commands;
+	(void)len;
+}
 void	process_input(const char *command)
 {
 	char	**split;
-	size_t	split_size;
-	size_t	i;
+	size_t	len;
 
 	split = ft_split(command, ' ');
-	split_size = count_split(split);
-	if (!split || !*split || !split_size || split_size == 1)
+	len = count_split(split);
+	if (!split || !*split || !len || len == 1)
 	{
 		free_split(split);
 		return ;
 	}
-	i = 0;
-	while (i < split_size)
-	{
-		if (ft_strcmp(split[i], ECHO_COMMAND))
-		{
-			printf("%s\n", split[i + 1]);
-			break ;
-		}
-		i++;
-	}
+	if (has_pipe(split, len))
+		process_commands_wp(split, len);
+	else
+		process_commands(split, len);
+	free_split(split);
 }
