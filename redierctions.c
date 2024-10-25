64a6c89dda5f005264a140fa9882dda13238c135
Hamad <AlmarzooqiH23@gmail.com>
2024-10-26 00:01:37 +0400

    Added all redirections functionality. Now i need to add the pipes functionality with redirections.

--- redierctions.c
+++ redierctions.c
@@ -6,84 +6,150 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 22:06:08 by hamad             #+#    #+#             */
-/*   Updated: 2024/10/23 21:15:07 by hamad            ###   ########.fr       */
+/*   Updated: 2024/10/25 23:54:49 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-void	redierct_to_file(char **bdir, char **commands)
+/**
+ * @brief	This function will redierct the output of the command that was pas-
+ * -sed to a file.
+ * @param	bdir	This holds the executable path.
+ * @param	commands	This holds the command that was passed in.
+ * @param	troa		Truncate or append.
+ * @return	void
+ */
+void	redierct_to_file(char **bdir, char **commands, int troa)
 {
 	int		fd;
 	int		childpid;
 	char	**tmp;
 	size_t	i;
 
-	fd = open(commands[count_split(commands) - 1], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd = open(commands[count_split(commands) - 1], O_WRONLY | O_CREAT | troa, 0664);
 	if (fd < 0)
 		return ;
 	childpid = fork();
 	if (!childpid)
 	{
-		dup2(fd, STDOUT_FILENO);
 		i = 0;
-		tmp = ft_subsplit(commands, REDICERTION_TO_FILE);
-		if (!tmp)
+		if (troa == O_TRUNC)
+			tmp = trim_command(ft_subsplit(commands, REDICERTION_TO_FILE));
+		else if (troa == O_APPEND)
+			tmp = trim_command(ft_subsplit(commands, APPEND_REDIRECTION));
+		if (!tmp || dup2(fd, STDOUT_FILENO) == -1)
 			exit(EXIT_FAILURE);
 		while (bdir[i])
 			ft_execute(bdir[i++], tmp);
-		free_split(tmp);
-		close(fd);
-		exit(EXIT_SUCCESS);
+		return (free_split(tmp), close(fd), exit(EXIT_SUCCESS));
 	}
 	else if (childpid > 0)
 		waitpid(childpid, NULL, 0);
 	close(fd);
 }
-
-void	redierct_to_input(char **bdir, char **commands, int fd[][2], size_t cpipe)
-{
-	(void)bdir;	
-	(void)commands;	
-	(void)fd;	
-	(void)cpipe;	
-}
-
-void	append_to_file(char **bdir, char **commands)
+/**
+ * @brief	This function will redirect a file or some data to the input of th-
+ * 			-e passed command.
+ * @param	bdir	This will hold the path of the executable.
+ * @param	commands	This holds the command that was passed by the user.
+ * @return	Void.
+*/
+void	redierct_to_input(char **bdir, char **commands)
 {
 	int		fd;
 	int		childpid;
 	char	**tmp;
 	size_t	i;
 
-	fd = open(commands[count_split(commands) - 1], O_WRONLY | O_CREAT | O_APPEND, 0664);
+	fd = open(commands[count_split(commands) - 1], O_RDONLY);
 	if (fd < 0)
 		return ;
 	childpid = fork();
 	if (!childpid)
 	{
-		dup2(fd, STDOUT_FILENO);
-		i = 0;
-		tmp = ft_subsplit(commands, APPEND_REDIRECTION);
-		if (!tmp)
+		tmp = trim_command(ft_subsplit(commands, REDIRECTION_TO_INPUT));
+		if (!tmp || dup2(fd, STDIN_FILENO) == -1)
 			exit(EXIT_FAILURE);
+		i = 0;
 		while (bdir[i])
 			ft_execute(bdir[i++], tmp);
-		free_split(tmp);
-		close(fd);
-		exit(EXIT_SUCCESS);
+		return (free_split(tmp), close(fd), exit(EXIT_SUCCESS));
 	}
 	else if (childpid > 0)
 		waitpid(childpid, NULL, 0);
-	close(fd);	
+	close(fd);
 }
+/**
+ * @brief	This function will mimic the behavior of heredoc.
+ * @param	bdir	This will hold the path of the executable.
+ * @param	commands	This holds the command that was passed by the user.
+ * @return	Void.
+ */
+void	heredoc_to_input(char **bdir, char **commands)
+{
+	char	*line;
+	char	*delimeter;
+	int		fd;
 
-void	herdoc_to_input(char **bdir, char **commands, int fd[][2], size_t cpipe)
-{	
-		(void)bdir;	
-	(void)commands;	
-	(void)fd;	
-	(void)cpipe;	
+	delimeter = commands[count_split(commands) - 1];
+	if (!delimeter || ft_strcmp(delimeter, HEREDOC_REDIRECTION))
+		return (perror("Bad delimeter"));
+	fd = open(TEMP_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	line = readline("> ");
+	while (1 && fd > 0 && !ft_strcmp(line, delimeter))
+	{
+		if (line)
+		{
+			ft_putendl_fd(line, fd);
+			free(line);
+		}
+		line = readline("> ");
+	}
+	if (line && !line[0])
+		free(line);
+	if (delimeter && !delimeter[0])
+		free(delimeter);
+	close(fd);
+	normal_process(bdir, commands, TEMP_FILE, HEREDOC_REDIRECTION);
+	unlink(TEMP_FILE);
+}
+/**
+ * @brief	This function will process a normal command passed by the user.
+ * @param	bdir		This will hold the path of the executable.
+ * @param	commands	This holds the command that was passed by the user.
+ * @param	fname		This will hold a filename. (Can be NULL)
+ * @param	dlmtr		This will hold a seperator in the command. (Can be-
+ * 						- NULL).
+ * @return				Void.
+ */
+void	normal_process(char **bdir, char **commands, char *fname, char *dlmtr)
+{
+	int		childpid;
+	char	**tmp;
+	size_t	i;
+	int		fd;
+
+	childpid = fork();
+	if (!childpid)
+	{
+		i = 0;
+		if (fname && fname[0] && dlmtr)
+		{
+			tmp = trim_command(ft_subsplit(commands, dlmtr));
+			fd = open(fname, O_RDONLY);
+			if (fd < 0 || dup2(fd, STDIN_FILENO) == -1)
+				return (perror("bad file descriptor"), exit(EXIT_FAILURE));
+			close(fd);
+		}
+		else
+			tmp = trim_command(commands);
+		while (tmp && tmp[0] && bdir[i])
+			ft_execute(bdir[i++], tmp);
+		return (free_split(tmp), exit(EXIT_SUCCESS));
+	}
+	else if (childpid > 0)
+		waitpid(childpid, NULL, 0);
 }
 
 /**
