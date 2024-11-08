cf813327230e7037904c7477a9f6e7f9d5106ee8
Hamad Almarzooqi <hamalmar@lab2r1s7.42abudhabi.ae>
2024-11-08 23:20:43 +0400

    Changed a lot of the functionality over here. The program can execute <redirection><file> <command>. Now i'm working on <redirection><file> <command> <redierection2><file2> <redirectionN><fileN>

--- command_utils2.c
+++ command_utils2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   command_utils2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
+/*   By: hamalmar <hamalmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 13:31:03 by hamad             #+#    #+#             */
-/*   Updated: 2024/11/07 22:50:43 by hamad            ###   ########.fr       */
+/*   Updated: 2024/11/08 23:12:44 by hamalmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@ int	is_bashsyntax(char **command)
 {
 	if (!command)
 		return (0);
-	if (ft_isprefix(command[0], REDICERTION_TO_FILE))
+	if (ft_strcmp(command[0], APPEND_REDIRECTION) || ft_isprefix(command[0], APPEND_REDIRECTION))
 		return (1);
-	if (ft_isprefix(command[0], REDIRECTION_TO_INPUT))
+	else if (ft_strcmp(command[0], HEREDOC_REDIRECTION) || ft_isprefix(command[0], HEREDOC_REDIRECTION))
 		return (1);
-	if (ft_isprefix(command[0], APPEND_REDIRECTION))
+	else if (ft_strcmp(command[0], REDICERTION_TO_FILE) || ft_isprefix(command[0], REDICERTION_TO_FILE))
 		return (1);
-	if (ft_isprefix(command[0], HEREDOC_REDIRECTION))
+	else if (ft_strcmp(command[0], REDIRECTION_TO_INPUT) || ft_isprefix(command[0], REDIRECTION_TO_INPUT))
 		return (1);
 	return (0);
 }
@@ -53,21 +53,33 @@ int	is_alone(char *s)
  * 						-ed to process_redirection.
  * @return	Void.
 */
-void	process_alone(char **bdir, char **command, size_t *i, int redirection, int (*rfd)[2])
+void	process_alone(char **bdir, char **command, size_t *i, int redirection, int *cflag, int (*fd)[2], int *cpipe)
 {
 	size_t	j;
+	size_t	k;
 	char	**sub_command;
+	size_t	nredirections;
 
 	*i = *i + 1;
-	j = *i;
-	while (command[j] != NULL && !(is_redirection(command[j]) >= 0))
+	k = *i;
+	sub_command = NULL;
+	nredirections = count_redirections(command);
+	j = 0;
+	while (j < nredirections)
+	{
+		while (command[k] != NULL && !(is_redirection(command[k]) >= 0))
+			k++;
+		if (*cflag)
+			sub_command = trim_command(ft_subnsplit(command, (*i) + 1, k));
+		if (*cflag && !sub_command)
+			return (perror("Failed to get subcommand."));
+		process_redirection(bdir, sub_command, command[*i], redirection, cflag, fd, cpipe);
+		if (*cflag && sub_command)
+			free_split(sub_command);
+		*i = k;
 		j++;
-	sub_command = trim_command(ft_subnsplit(command, (*i) + 1, j - 1));
-	if (!sub_command)
-		return (perror("Failed to get subcommand."));
-	process_redirection(bdir, sub_command, command[*i], redirection, rfd, *i);
-	free_split(sub_command);
-	*i = j;
+	}
+	*cpipe = *cpipe + 1;
 }
 
 /**
@@ -81,7 +93,7 @@ void	process_alone(char **bdir, char **command, size_t *i, int redirection, int 
  * 						-ed to process_redirection.
  * @return	Void.
 */
-void	process_not_alone(char **bdir, char **command, size_t *i, int redirection, int (*rfd)[2])
+void	process_not_alone(char **bdir, char **command, size_t *i, int redirection)
 {
 	size_t	j;
 	char	**sub_command;
@@ -96,32 +108,31 @@ void	process_not_alone(char **bdir, char **command, size_t *i, int redirection, 
 	sub_command = trim_command(ft_subnsplit(command, (*i) + 1, j - 1));
 	if (!sub_command)
 		return (perror("Failed to get subcommand."));
-	process_redirection(bdir, sub_command, fname, redirection, rfd, *i);
+	process_redirection(bdir, sub_command, fname, redirection, 0, NULL, NULL);
 	free_split(sub_command);
 	*i = j;
 	free(fname);
 }
 
-void	process_bash(char **bdir, char **command, int (*fd)[2])
+void	process_bash(char **bdir, char **command, int (*fd)[2], int *cpipe)
 {
 	size_t	i;
 	int		redirection;
-	int		(*rfd)[2];
+	int		cflag;
 
-	(void)fd;
 	if (!command)
 		return ;
-	if (init_pipes(&rfd, count_redirections(command)) == -1)
-		return (perror("Failed to init pipes"));
 	i = 0;
+	cflag = 1;
 	while (command[i] != NULL && i < count_split(command))
-	{ 
+	{
 		redirection = is_redirection(command[i]);
 		if (redirection >= 0 && is_alone(command[i]))
-			process_alone(bdir, command, &i, redirection, rfd);
+		{
+			process_alone(bdir, command, &i, redirection, &cflag, fd, cpipe);
+		}
 		else if (redirection >= 0 && !is_alone(command[i]))
-			process_not_alone(bdir, command, &i, redirection, rfd);
+			process_not_alone(bdir, command, &i, redirection);
 		i++;
 	}
-	close_pipes(rfd, count_redirections(command));
 }
