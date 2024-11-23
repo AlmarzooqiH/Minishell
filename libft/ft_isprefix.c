90a7eecd2d110adac6c92728029031165620bd91
Hamad <AlmarzooqiH23@gmail.com>
2024-11-24 02:14:33 +0400

    Added an edge condition where if the s1 and prefix length are not the same it will evaluate to 0.

--- libft/ft_isprefix.c
+++ libft/ft_isprefix.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isprefix.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hamalmar <hamalmar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 12:52:26 by hamad             #+#    #+#             */
-/*   Updated: 2024/11/08 19:01:17 by hamalmar         ###   ########.fr       */
+/*   Updated: 2024/11/21 04:45:06 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_isprefix(const char *s1, const char *prefix)
 {
 	size_t	i;
 
-	if (!s1 || !prefix)
+	if (!s1 || !prefix || ft_strlen(s1) < ft_strlen(prefix))
 		return (0);
 	i = 0;
 	while (s1[i] != '\0'&& prefix[i] != '\0' && prefix[i] == s1[i])
