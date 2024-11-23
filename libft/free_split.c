4b819e922e2746a55d1d5a4554d2e156bf177794
Hamad <AlmarzooqiH23@gmail.com>
2024-11-24 02:13:30 +0400

    Made it non varadic to we get no issues when we submit.

--- libft/free_split.c
+++ libft/free_split.c
@@ -6,37 +6,23 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 13:49:25 by truedevelop       #+#    #+#             */
-/*   Updated: 2024/11/14 01:14:30 by hamad            ###   ########.fr       */
+/*   Updated: 2024/11/16 18:12:26 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	free_split(size_t len, ...)
+void	free_split(char **s)
 {
 	size_t	i;
-	size_t	j;
-	va_list	args;
-	char	**s;
 
-	if (len == 0)
+	if (!s)
 		return ;
-	va_start(args, len);
 	i = 0;
-	while (i < len)
+	while (s[i])
 	{
-		s = va_arg(args, char **);
-		if (s)
-		{
-			j = 0;
-			while (s[j])
-			{
-				free(s[j]);
-				j++;
-			}
-			free(s);
-		}
+		free(s[i]);
 		i++;
 	}
-	va_end(args);
+	free(s);
 }
