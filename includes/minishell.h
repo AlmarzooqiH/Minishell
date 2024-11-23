2eb04bdcf508ac8edd079243d4155d1c64135669
Hamad <AlmarzooqiH23@gmail.com>
2024-11-24 02:12:03 +0400

    Added new function prototypes, but will remove the non used functions later on.

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 20:22:44 by hamad             #+#    #+#             */
-/*   Updated: 2024/11/16 17:15:36 by hamad            ###   ########.fr       */
+/*   Updated: 2024/11/23 22:04:47 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,32 +28,39 @@
 # include <termios.h>
 //If we decide to add clear function, we just need to printf this: \033[H\033[J
 void	process_input(const char *command);
-void	process_commands(char ***commands, size_t len);
-void	process_commands_wp(char ***commands, size_t len);
-void	process_echo(char **commands, size_t len);
+int		validate_command(char ***command);
+void	init(t_commands *cmds, const char *command);
+void	set_redirectons(t_commands *cmds);
+int		count_tokens(char **tokens);
+void	free_cmds(t_commands *cmds);
+void	set_isbash(t_commands *cmds);
+void	set_files(t_commands *cmds);
+void	print_struct(t_commands *cmds);
+void	execute_binary(t_commands *cmds);
+void    execute_cmd(t_commands *cmds);
+void	execute_bash(t_commands *cmds);
+char	**extract_command(t_commands *cmds);
+// void	process_echo(char **commands, size_t len);
 int		has_flag(char *flag, char *flag_in);
 int		has_pipe(char *commands);
 int		ft_execute(char *pvar, char **commands);
 int		ft_execute2(char **commands);
 void	print_stdout(int fd);
-char	**create_argv(int fd);
-long	count_lines(int fd);
 void	get_tokens(char *command, char ***tokens, char split);
 void	free_tokens(char ***tokens, int n_tokens);
-void	execute_binary(char ***commands, size_t size);
-void	one_command(char **bdir, char **commands, int (*fd)[2], size_t *cpipe);
-void	process_parent(char **bdir, char **commands, int (*fd)[2],
-			size_t cpipe);
+// void	one_command(char **bdir, char **commands, int (*fd)[2], size_t *cpipe);
+// void	process_parent(char **bdir, char **commands, int (*fd)[2],
+			// size_t cpipe);
 void	close_pipes(int (*fd)[2], size_t npipes);
 void	close_pipe(int *fd, int which);
 char	**trim_command(char **commands);
 int		dup_pipes(int (*fd)[2], size_t cpipe, int to);
-int		init_pipes(int (**fd)[2], size_t clen, size_t t_redir);
+int		init_pipes(int (**fd)[2], int clen);
 int		is_redirection(char *command);
-void	redierct_to_file(char **bdir, char **commands, char *fname, int (*fd)[2], size_t *cpipe, int troa);
-void	redierct_to_input(char **bdir, char **commands, char *fname, int (*fd)[2], size_t *cpipe);
-void	heredoc_to_input(char **bdir, char **commands, char *dlmtr, int (*fd)[2], size_t *cpipe);
-void	normal_process(char **bdir, char **commands, char *fname, int (*fd)[2], size_t *cpipe);
+// void	redierct_to_file(char **bdir, char **commands, char *fname, int (*fd)[2], size_t *cpipe, int troa);
+// void	redierct_to_input(char **bdir, char **commands, char *fname, int (*fd)[2], size_t *cpipe);
+// void	heredoc_to_input(char **bdir, char **commands, char *dlmtr, int (*fd)[2], size_t *cpipe);
+// void	normal_process(char **bdir, char **commands, char *fname, int (*fd)[2], size_t *cpipe);
 size_t	count_redirections(char **command);
 int		is_bashsyntax(char **command);
 int		is_alone(char *s);
@@ -61,7 +68,7 @@ void	process_bash(char **bdir, char **command, int (*fd)[2], size_t *cpipe, size
 void	process_redirection(char **bdir, char **command, char *fname, int re, int *cflag, int (*fd)[2], size_t *cpipe);
 void	process_alone(char **bdir, char **command, size_t *i, int redirection, int *cflag, int (*fd)[2], size_t *cpipe);
 void	process_not_alone(char **bdir, char **command, size_t *i, int redirection, int *cflag, int (*fd)[2], size_t *cpipe);
-char	*get_filename(char *command, int redirection);
+char	*gfn(char *command, int redirection);
 size_t	get_total_rediractions(char ***commands);
 void	redierct_to_files(char *fname, int re, int (*fd)[2], size_t *cpipe);
 #endif
