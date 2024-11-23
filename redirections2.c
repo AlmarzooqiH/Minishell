7f7280cabe0d19f3ccb4fd79c3d19e553ec84b1b
Hamad <AlmarzooqiH23@gmail.com>
2024-11-24 02:16:46 +0400

    Commented on most functions, will be reworked later.

--- redirections2.c
+++ redirections2.c
@@ -6,7 +6,7 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 20:05:16 by hamad             #+#    #+#             */
-/*   Updated: 2024/11/13 19:40:02 by hamad            ###   ########.fr       */
+/*   Updated: 2024/11/18 21:04:37 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,26 +52,26 @@ size_t	count_redirections(char **command)
  * are no commands.
  * @return Void.
 */
-void	process_redirection(char **bdir, char **command, char *fname, int re, int *cflag, int (*fd)[2], size_t *cpipe)
-{
-	if (!command || !command[0])
-		return (perror("Command is empty."));
-	if (*cflag)
-	{
-		*cflag = 0;
-		if (re == e_redirection_to_file)
-			return (redierct_to_file(bdir, command, fname, fd, cpipe, O_TRUNC));
-		else if (re == e_redirection_to_input)
-			return (redierct_to_input(bdir, command, fname, fd, cpipe));
-		else if (re == e_append_redirection)
-			return (redierct_to_file(bdir, command, fname, fd, cpipe, O_APPEND));
-		else if (re == e_heredoc_redirection)
-			return (heredoc_to_input(bdir, command, fname, fd, cpipe));
-		else
-			return (normal_process(bdir, command, NULL, NULL, NULL));
-	}
-	redierct_to_files(fname, re, fd, cpipe);
-}
+// void	process_redirection(char **bdir, char **command, char *fname, int re, int *cflag, int (*fd)[2], size_t *cpipe)
+// {
+// 	if (!command || !command[0])
+// 		return (perror("Command is empty."));
+// 	if (*cflag)
+// 	{
+// 		*cflag = 0;
+// 		if (re == e_redirection_to_file)
+// 			return (redierct_to_file(bdir, command, fname, fd, cpipe, O_TRUNC));
+// 		else if (re == e_redirection_to_input)
+// 			return (redierct_to_input(bdir, command, fname, fd, cpipe));
+// 		else if (re == e_append_redirection)
+// 			return (redierct_to_file(bdir, command, fname, fd, cpipe, O_APPEND));
+// 		else if (re == e_heredoc_redirection)
+// 			return (heredoc_to_input(bdir, command, fname, fd, cpipe));
+// 		else
+// 			return (normal_process(bdir, command, NULL, NULL, NULL));
+// 	}
+// 	redierct_to_files(fname, re, fd, cpipe);
+// }
 /**
  * @brief This function will count how many redierctions we got inside the com-
  * -mands.
@@ -92,29 +92,29 @@ size_t	get_total_rediractions(char ***commands)
 	return (total);
 }
 
-void	redierct_to_files(char *fname, int re, int (*fd)[2], size_t *cpipe)
-{
-	int		file;
-	char	*line;
+// void	redierct_to_files(char *fname, int re, int (*fd)[2], size_t *cpipe)
+// {
+// 	int		file;
+// 	char	*line;
 
-	if (!fname)
-		return (perror("Failed to get filename."));
-	file = 0;
-	if (re == e_redirection_to_file)
-		file = open(fname, O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMISSIONS);
-	else if (re == e_append_redirection)
-		file = open(fname, O_WRONLY | O_CREAT | O_APPEND, FILE_PERMISSIONS);
-	if (file < 0)
-		return (perror("Failed to open file."));
-	line = get_next_line(fd[*cpipe][0]);
-	while (line && line[0] != '\0')
-	{
-		write(file, line, ft_strlen(line));
-		free(line);
-		line = get_next_line(fd[*cpipe][0]);
-	}
-	if (line)
-		free(line);
-	close(file);
-	close_pipe(fd[*cpipe], 2);
-}
+// 	if (!fname)
+// 		return (perror("Failed to get filename."));
+// 	file = 0;
+// 	if (re == e_redirection_to_file)
+// 		file = open(fname, O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMISSIONS);
+// 	else if (re == e_append_redirection)
+// 		file = open(fname, O_WRONLY | O_CREAT | O_APPEND, FILE_PERMISSIONS);
+// 	if (file < 0)
+// 		return (perror("Failed to open file."));
+// 	line = get_next_line(fd[*cpipe][0]);
+// 	while (line && line[0] != '\0')
+// 	{
+// 		write(file, line, ft_strlen(line));
+// 		free(line);
+// 		line = get_next_line(fd[*cpipe][0]);
+// 	}
+// 	if (line)
+// 		free(line);
+// 	close(file);
+// 	close_pipe(fd[*cpipe], 2);
+// }
