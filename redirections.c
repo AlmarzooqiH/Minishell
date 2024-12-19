9883ca930dafbfe38d2f18e2dc50266c9d3fa6bb
Hamad <AlmarzooqiH23@gmail.com>
2024-12-19 17:15:20 +0400

    Here is the redirection functionality.

--- redirections.c
+++ redirections.c
@@ -6,7 +6,7 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 22:06:08 by hamad             #+#    #+#             */
-/*   Updated: 2024/11/18 17:25:57 by hamad            ###   ########.fr       */
+/*   Updated: 2024/12/19 17:09:13 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,13 +24,87 @@
  */
 int	is_redirection(char *command)
 {
-	if (ft_strcmp(command, APPEND_REDIRECTION) || ft_isprefix(command, APPEND_REDIRECTION))
-		return (e_append_redirection);
-	else if (ft_strcmp(command, HEREDOC_REDIRECTION) || ft_isprefix(command, HEREDOC_REDIRECTION))
-		return (e_heredoc_redirection);
-	else if (ft_strcmp(command, REDICERTION_TO_FILE) || ft_isprefix(command, REDICERTION_TO_FILE))
-		return (e_redirection_to_file);
-	else if (ft_strcmp(command, REDIRECTION_TO_INPUT) || ft_isprefix(command, REDIRECTION_TO_INPUT))
-		return (e_redirection_to_input);
+	if (ft_strcmp(command, APPEND_REDIRECTION)
+		|| ft_isprefix(command, APPEND_REDIRECTION))
+		return (e_ar);
+	else if (ft_strcmp(command, HEREDOC_REDIRECTION)
+		|| ft_isprefix(command, HEREDOC_REDIRECTION))
+		return (e_hdr);
+	else if (ft_strcmp(command, REDICERTION_TO_FILE)
+		|| ft_isprefix(command, REDICERTION_TO_FILE))
+		return (e_rtf);
+	else if (ft_strcmp(command, REDIRECTION_TO_INPUT)
+		|| ft_isprefix(command, REDIRECTION_TO_INPUT))
+		return (e_rti);
 	return (-1);
 }
+
+/**
+ * @brief This function will create and open files and assign file descriptors
+ * to cmds->fd[position of the fd].
+ * @param cmds This holds the commands struct.
+ * @return Void.
+ */
+void	create_files(t_commands *cmds)
+{
+	int	i;
+
+	i = 0;
+	while (i < count_tokens(cmds->cmds[cmds->cc]))
+	{
+		if (cmds->rd[cmds->cc][i] == e_rtf)
+			cmds->fd[cmds->cfd] = open(cmds->files[cmds->cf], TRUNC, PERMS);
+		else if (cmds->rd[cmds->cc][i] == e_rti && !check_file(cmds))
+			cmds->fd[cmds->cfd] = open(cmds->files[cmds->cf], RD , PERMS);
+		else if (cmds->rd[cmds->cc][i] == e_ar)
+			cmds->fd[cmds->cfd] = open(cmds->files[cmds->cf], APPEND, PERMS);
+		else if (cmds->rd[cmds->cc][i] == e_hdr)
+			cmds->hdp = cmds->cf;
+		else if (cmds->rd[cmds->cc][i] == -1)
+		{
+			i++;
+			continue ;
+		}
+		if (cmds->rd[cmds->cc][i] == e_rti)
+			cmds->rtip = cmds->cfd;
+		i++;
+		cmds->cf++;
+		cmds->cfd++;
+		cmds->cr = i;
+		cmds->efdp = cmds->cfd;
+	}
+}
+
+/**
+ * @brief This function will process the redirections.
+ * @param cmds This holds the commands struct.
+ * @return Void.
+ */
+void	process_redir(t_commands *cmds)
+{
+	int		i;
+	char	**scmd;
+
+	create_files(cmds);
+	scmd = extract_command(cmds);
+	if (!scmd)
+		return (perror("Failed to extract the command"), exit(EF));
+	if (cmds->hdp >= 0 && process_heredoc(cmds))
+	{
+		cmds->rd[cmds->cc][cmds->hdp] = open(TEMP_FILE, O_RDONLY, PERMS);
+		if (dup2(cmds->rd[cmds->cc][cmds->hdp], SIN) == -1)
+			return (perror("Failed to dup2(hdr, SIN)"), exit(EF));
+	}
+	if (cmds->rtip >= 0 && dup2(cmds->fd[cmds->rtip], SIN) == -1)
+		return (perror("Failed to dup2(rtip, SIN)"), exit(EF));
+	if (cmds->rd[cmds->cc][cmds->cr - 1] == e_rtf
+		|| cmds->rd[cmds->cc][cmds->cr - 1] == e_ar)
+	{
+		if (dup2(cmds->fd[cmds->cfd - 1], SOUT) == -1)
+			return (perror("Failed to dup2(rtf or rti, SOUT)"), exit(EF));
+	}
+	i = 0;
+	while (cmds->bpath[i] && ft_execute(cmds->bpath[i], scmd))
+			i++;
+	return (close_files(cmds), free_split(scmd));
+}	
--- redirections2.c
+++ redirections2.c
@@ -6,7 +6,7 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/26 20:05:16 by hamad             #+#    #+#             */
-/*   Updated: 2024/11/18 21:04:37 by hamad            ###   ########.fr       */
+/*   Updated: 2024/12/18 01:05:33 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@
  * @param	command This holds the command that was passed.
  * @return	Will return the number of redirections within the command.
  */
-size_t	count_redirections(char **command)
+int	count_redirections(char **command)
 {
-	size_t	n;
-	size_t	i;
+	int	n;
+	int	i;
 
 	if (!command)
 		return (0);
@@ -29,59 +29,33 @@ size_t	count_redirections(char **command)
 	n = 0;
 	while (command[i] != NULL)
 	{
-		if (ft_strcmp(command[i], APPEND_REDIRECTION) || ft_isprefix(command[i], APPEND_REDIRECTION))
+		if (ft_strcmp(command[i], APPEND_REDIRECTION)
+			|| ft_isprefix(command[i], APPEND_REDIRECTION))
 			n++;
-		else if (ft_strcmp(command[i], HEREDOC_REDIRECTION) || ft_isprefix(command[i], HEREDOC_REDIRECTION))
+		else if (ft_strcmp(command[i], HEREDOC_REDIRECTION)
+			|| ft_isprefix(command[i], HEREDOC_REDIRECTION))
 			n++;
-		else if (ft_strcmp(command[i], REDICERTION_TO_FILE) || ft_isprefix(command[i], REDICERTION_TO_FILE))
+		else if (ft_strcmp(command[i], REDICERTION_TO_FILE)
+			|| ft_isprefix(command[i], REDICERTION_TO_FILE))
 			n++;
-		else if (ft_strcmp(command[i], REDIRECTION_TO_INPUT) || ft_isprefix(command[i], REDIRECTION_TO_INPUT))
+		else if (ft_strcmp(command[i], REDIRECTION_TO_INPUT)
+			|| ft_isprefix(command[i], REDIRECTION_TO_INPUT))
 			n++;
 		i++;
 	}
 	return (n);
 }
-/**
- * @brief	This function will process any rediection based on the user input.
- * @param	bdir	Binary/executable path.
- * @param	command	This will hold in the current command.
- * @param	fname	This will hold the filename(Can be NULL).
- * @param	re		This holds the type of redierction we got. If -1 this mean-
- * -s there are no redierections and will be processed normally.
- * @param	cflag	This will hold the 1 if there is a command and 0 if there
- * are no commands.
- * @return Void.
-*/
-// void	process_redirection(char **bdir, char **command, char *fname, int re, int *cflag, int (*fd)[2], size_t *cpipe)
-// {
-// 	if (!command || !command[0])
-// 		return (perror("Command is empty."));
-// 	if (*cflag)
-// 	{
-// 		*cflag = 0;
-// 		if (re == e_redirection_to_file)
-// 			return (redierct_to_file(bdir, command, fname, fd, cpipe, O_TRUNC));
-// 		else if (re == e_redirection_to_input)
-// 			return (redierct_to_input(bdir, command, fname, fd, cpipe));
-// 		else if (re == e_append_redirection)
-// 			return (redierct_to_file(bdir, command, fname, fd, cpipe, O_APPEND));
-// 		else if (re == e_heredoc_redirection)
-// 			return (heredoc_to_input(bdir, command, fname, fd, cpipe));
-// 		else
-// 			return (normal_process(bdir, command, NULL, NULL, NULL));
-// 	}
-// 	redierct_to_files(fname, re, fd, cpipe);
-// }
+
 /**
  * @brief This function will count how many redierctions we got inside the com-
  * -mands.
  * @param	commands	This holds the commands that was passed.
  * @return	Will return the total number of redierctions.
 */
-size_t	get_total_rediractions(char ***commands)
+int	get_total_rediractions(char ***commands)
 {
-	size_t	i;
-	size_t	total;
+	int	i;
+	int	total;
 
 	if (!commands || !commands[0])
 		return (0);
@@ -92,29 +66,54 @@ size_t	get_total_rediractions(char ***commands)
 	return (total);
 }
 
-// void	redierct_to_files(char *fname, int re, int (*fd)[2], size_t *cpipe)
-// {
-// 	int		file;
-// 	char	*line;
+void	close_files(t_commands *cmds)
+{
+	int	i;
 
-// 	if (!fname)
-// 		return (perror("Failed to get filename."));
-// 	file = 0;
-// 	if (re == e_redirection_to_file)
-// 		file = open(fname, O_WRONLY | O_CREAT | O_TRUNC, FILE_PERMISSIONS);
-// 	else if (re == e_append_redirection)
-// 		file = open(fname, O_WRONLY | O_CREAT | O_APPEND, FILE_PERMISSIONS);
-// 	if (file < 0)
-// 		return (perror("Failed to open file."));
-// 	line = get_next_line(fd[*cpipe][0]);
-// 	while (line && line[0] != '\0')
-// 	{
-// 		write(file, line, ft_strlen(line));
-// 		free(line);
-// 		line = get_next_line(fd[*cpipe][0]);
-// 	}
-// 	if (line)
-// 		free(line);
-// 	close(file);
-// 	close_pipe(fd[*cpipe], 2);
-// }
+	i = cmds->bfdp;
+	unlink(TEMP_FILE);
+	while (i < cmds->efdp)
+		close(cmds->fd[i++]);
+	cmds->bfdp = cmds->efdp;
+}
+
+/**
+ * @brief This function will simulate the heredoc redirection.
+ * @param cmds This holds the commands struct.
+ * @return 0 if failed
+ * @return 1 if succeeded.
+ */
+int process_heredoc(t_commands *cmds)
+{
+    int     fd;
+    char    *tmp;
+
+	if (cmds->hdp < 0)
+		return (0);
+    fd = open(TEMP_FILE, TRUNC, PERMS);
+    if (fd < 0)
+		return (perror("Failed to create temp file"), 0);
+    tmp = readline("Martho Heredoc> ");
+    while (tmp != NULL && !ft_strcmp(cmds->files[cmds->hdp], tmp))
+    {
+		if (tmp)
+		{
+			ft_putendl_fd(tmp, fd);
+        	free(tmp);
+		}
+        tmp = readline("Martho Heredoc> ");
+    }
+    if (tmp)
+		free(tmp);
+    return (close(fd), 1);
+}
+
+/**
+ * @brief This function will check if the file is it's exsistance.
+ * @param cmds This holds the command struct.
+ * @return Whatever access() returns.
+ */
+int	check_file(t_commands *cmds)
+{
+	return (access(cmds->files[cmds->cf], F_OK));
+}
