8bf558390bd47495bd4edf4ffaeb9f989e900bd4
Hamad <AlmarzooqiH23@gmail.com>
2024-12-19 17:12:44 +0400

    Added new macros and added/modifed the commands struct.

--- includes/commands.h
+++ includes/commands.h
@@ -6,7 +6,7 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 12:24:25 by hamad             #+#    #+#             */
-/*   Updated: 2024/11/25 22:31:22 by hamad            ###   ########.fr       */
+/*   Updated: 2024/12/19 15:05:23 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,15 @@
 # define HEREDOC_REDIRECTION "<<\0"
 # define NL_FLAG "-n\0"
 # define TEMP_FILE "/tmp/Martho_Heredoc_temp.txt\0"
-# define FILE_PERMISSIONS 0664
+# define PERMS 0664
 
 # define SIN STDIN_FILENO
 # define SOUT STDOUT_FILENO
 # define EF EXIT_FAILURE
 # define ES EXIT_SUCCESS
+# define APPEND O_CREAT | O_RDWR | O_APPEND
+# define TRUNC O_CREAT | O_RDWR | O_TRUNC
+# define RD O_RDONLY
 
 /**
  * @brief	This structure will hold the passed in command that will be prepro-
@@ -39,16 +42,21 @@
  * @var	bpath	Will hold the splited PATH variable.
  * @var	cmds	This will hold the tokenized commands.
  * @var	files	This will hold the files that we will redirect to.
- * @var	redir	This will hold the type of the redirection.
- * @var	is_bash	This will hold a value of 1 or 0 if the current command is in bash or not.
- * @var	fd		This will hold the pipelines.
+ * @var	rd	This will hold the type of the redirection.
+ * @var	is_bash	This will hold a value of 1 or 0 if the current command is 
+ * in bash or not.
+ * @var	p		This will hold the pipelines.
  * @var	nscmds	This will hold the number of the commands that we have.
- * @var	npipes	WIll be calculatedd when we init the pipes.
- * @var	nredir	Will be calculated when we init the pipes.
+ * @var	npipes	Will be calculatedd when we init the pipes.
+ * @var	nre	Will be calculated when we init the pipes.
  * @var	cpipe	Current pipe.
  * @var	ccmd	Current command.
- * @var	cfile	Current file.
- * @var	cred	Current redirection.
+ * @var	cf	Current file.
+ * @var	cr	Current redirection.
+ * @var	hdp	Heredoc position.
+ * @var	rtip	Redirection to input position.
+ * @var	bfdp	Beggening fd position.
+ * @var	efdp	Ending of fd position.
  */
 typedef struct s_commands
 {
@@ -56,23 +64,36 @@ typedef struct s_commands
 	char	**bpath;
 	char	***cmds;
 	char	**files;
-	int		**redir;
+	int		**rd;
 	int		*is_bash;
-	int		(*fd)[2];
+	int		(*p)[2];
+	int		*fd;
 	int		nscmds;
 	int		npipes;
-	int		nredir;
-	int		cpipe;
-	int		ccmd;
-	int		cfile;
-	int		cred;
+	int		nre;
+	int		cp;
+	int		cc;
+	int		cf;
+	int		cr;
+	int		cfd;
+	int		hdp;
+	int		rtip;
+	int		bfdp;
+	int		efdp;
 }	t_commands;
 
+/**
+ * @brief This will hold the type of the redirection.
+ * @var e_rtf Redirection to file.
+ * @var e_rti Redirection to input.
+ * @var e_ar Append redirection.
+ * @var e_hdr Heredoc redirection.
+ */
 enum	e_operations
 {
-	e_redirection_to_file,
-	e_redirection_to_input,
-	e_append_redirection,
-	e_heredoc_redirection
+	e_rtf,
+	e_rti,
+	e_ar,
+	e_hdr
 };
 #endif
