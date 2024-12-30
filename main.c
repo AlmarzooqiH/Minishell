5a30bc929bebfc4db91cf6e3621884315fdfc349
Hamad <AlmarzooqiH23@gmail.com>
2024-12-30 20:03:28 +0400

    Added the envp to print the enviorment vairales.

--- main.c
+++ main.c
@@ -6,22 +6,24 @@
 /*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/22 20:41:00 by hamad             #+#    #+#             */
-/*   Updated: 2024/12/23 17:33:01 by hamad            ###   ########.fr       */
+/*   Updated: 2024/12/30 17:02:15 by hamad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-int	main(void)
+int	main(int ac, char **av, char **envp)
 {
 	char	*line;
 
+	(void)ac;
+	(void)av;
 	while (1)
 	{
 		line = readline("\e[32mMartho shell\e[0m# ");
 		if (line)
 		{
-			process_input(line);
+			process_input(line, envp);
 			add_history(line);
 			free(line);
 		}
