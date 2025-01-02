3e2de78719353b325f5ad16e6d1dfffda443f029
Mohamed Munawwar <mohamedmunawwar9@gmail.com>
2025-01-02 15:30:39 +0400

    Refactor builtin_export functions for improved readability and maintainability

--- builtin_export.c
+++ builtin_export.c
@@ -3,82 +3,92 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_export.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hamad <hamad@student.42.fr>                +#+  +:+       +#+        */
+/*   By: root <root@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/01 20:18:19 by hamad             #+#    #+#             */
-/*   Updated: 2025/01/01 23:35:56 by hamad            ###   ########.fr       */
+/*   Updated: 2025/01/02 15:27:57 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-void	print_envp(char **envp)
+void print_envp(char **envp)
 {
-	int	i;
+    int i;
 
-	i = 0;
-	while (envp[i])
-	{
-		printf("declare -x %s\n", envp[i]);
-		i++;
-	}
+    i = 0;
+    while (envp[i])
+    {
+        printf("declare -x %s\n", envp[i]);
+        i++;
+    }
 }
 
-int	is_valid_identifier(t_commands *cmds, int i)
+int is_valid_identifier(t_commands *cmds, int i)
 {
-	int	j;
+    int j;
 
-	j = 1;
-	if (!ft_isalpha(cmds->c[cmds->cc][i][0]) && cmds->c[cmds->cc][i][0] != '_')
-		return (0);
-	while (cmds->c[cmds->cc][i][j] && cmds->c[cmds->cc][i][j] != '=')
-	{
-		if (!ft_isalnum(cmds->c[cmds->cc][i][j])
-			&& cmds->c[cmds->cc][i][j] != '_')
-			return (0);
-		j++;
-	}
-	return (1);
+    if (!isalpha(cmds->c[cmds->cc][i][0]) && cmds->c[cmds->cc][i][0] != '_')
+        return (0);
+    j = 1;
+    while (cmds->c[cmds->cc][i][j] && cmds->c[cmds->cc][i][j] != '=')
+    {
+        if (!isalnum(cmds->c[cmds->cc][i][j]) && cmds->c[cmds->cc][i][j] != '_')
+            return (0);
+        j++;
+    }
+    return (1);
 }
 
-void	update_envp(t_commands *cmds, int i)
+void update_envp(t_commands *cmds, int i)
 {
-	int	j;
+    int j;
+    char *name;
+    char *equal_pos;
 
-	j = 0;
-	while (cmds->envp[j])
-	{
-		if (ft_strncmp(cmds->c[cmds->cc][i],
-			cmds->envp[j], ft_strlen(cmds->c[cmds->cc][i])) == 0)
-		{
-			free(cmds->envp[j]);
-			cmds->envp[j] = ft_strdup(cmds->c[cmds->cc][i]);
-			return ;
-		}
-		j++;
-	}
-	cmds->envp[j] = ft_strdup(cmds->c[cmds->cc][i]);
-	cmds->envp[j + 1] = NULL;
+    equal_pos = ft_strchr(cmds->c[cmds->cc][i], '=');
+    if (!equal_pos)
+        return;
+    name = ft_substr(cmds->c[cmds->cc][i], 0, equal_pos - cmds->c[cmds->cc][i]);
+    j = 0;
+    while (cmds->envp[j])
+    {
+        if (ft_strncmp(name, cmds->envp[j], ft_strlen(name)) == 0 &&
+            cmds->envp[j][ft_strlen(name)] == '=')
+        {
+            free(cmds->envp[j]);
+            cmds->envp[j] = ft_strdup(cmds->c[cmds->cc][i]);
+            free(name);
+            return;
+        }
+        j++;
+    }
+    cmds->envp[j] = ft_strdup(cmds->c[cmds->cc][i]);
+    cmds->envp[j + 1] = NULL;
+    free(name);
 }
 
-void	builtin_export(t_commands *cmds)
+void builtin_export(t_commands *cmds)
 {
-	int	i;
+    int i;
 
-	if (count_tokens(cmds->c[cmds->cc]) == 1)
-		return (print_envp(cmds->envp));
-	i = 1;
-	while (cmds->c[cmds->cc][i])
-	{
-		if (!is_valid_identifier(cmds, i))
-		{
-			printf("export: `%s': not a valid identifier\n",
-				cmds->c[cmds->cc][i]);
-			cmds->es = 1;
-		}
-		else
-			update_envp(cmds, i);
-		i++;
-	}
-	cmds->es = 0;
+    if (!cmds->c[cmds->cc][1])
+    {
+        print_envp(cmds->envp);
+        return;
+    }
+    i = 1;
+    while (cmds->c[cmds->cc][i])
+    {
+        if (!is_valid_identifier(cmds, i))
+        {
+            printf("export: `%s': not a valid identifier\n",
+                   cmds->c[cmds->cc][i]);
+            cmds->es = 1;
+        }
+        else
+            update_envp(cmds, i);
+        i++;
+    }
+    cmds->es = 0;
 }
