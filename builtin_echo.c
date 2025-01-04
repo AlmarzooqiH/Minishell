a0547872bb15f79636aecf22124488b4b28eae71
Mohamed Munawwar <mohamedmunawwar9@gmail.com>
2025-01-04 18:24:45 +0400

    Refactor builtin_echo and related functions for improved readability and consistency

--- builtin_echo.c
+++ builtin_echo.c
@@ -6,36 +6,35 @@
  * @param str The string containing the environment variable.
  * @return Allocated string containing the expanded value or empty string if env var doesn't exist.
  */
-char *expand_env(t_commands *cmds, const char *str)
+char	*expand_env(t_commands *cmds, const char *str)
 {
-    char *tmp;
-    char *env;
-    char *value = NULL;
-    
-    tmp = ft_substr(str, 1, ft_strlen(str));
-    if (!tmp)
-        return (NULL);
-        
-    env = ft_strtrim(tmp, "$\"");
-    free(tmp);
-    if (!env)
-        return (NULL);
-        
-    int i = 0;
-    while (cmds->envp[i])
-    {
-        if (ft_isprefix(cmds->envp[i], env))
-        {
-            value = ft_strdup(ft_strchr(cmds->envp[i], '=') + 1);
-            break;
-        }
-        i++;
-    }
-    free(env);
-    // Return empty string if environment variable is not found
-    if (!value)
-        return (ft_strdup(""));
-    return (value);
+	char	*tmp;
+	char	*env;
+    char	*value;
+    int		i;
+	
+	i = 0;
+	value = NULL;
+	tmp = ft_substr(str, 1, ft_strlen(str));
+	if (!tmp)
+		return (NULL);
+	env = ft_strtrim(tmp, "$\"");
+	free(tmp);
+	if (!env)
+		return (NULL);
+	while (cmds->envp[i])
+	{
+		if (ft_isprefix(cmds->envp[i], env))
+		{
+			value = ft_strdup(ft_strchr(cmds->envp[i], '=') + 1);
+			break;
+		}
+		i++;
+	}
+	free(env);
+	if (!value)     // Return empty string if environment variable is not found
+		return (ft_strdup(""));
+	return (value);
 }
 
 /**
@@ -43,26 +42,26 @@ char *expand_env(t_commands *cmds, const char *str)
  * @param cmds Command structure
  * @param str String to process
  */
-void print_double_quoted_content(t_commands *cmds, const char *str)
+void	print_double_quoted_content(t_commands *cmds, const char *str)
 {
-    int i = 1;  // Skip first quote
-    
+    int		i;  // Skip first quote
+    char	*value;
+	i = 1;
     while (str[i] && str[i] != '\"')
-    {
-        if (str[i] == '$' && str[i + 1] && str[i + 1] != '\"')
-        {
-            char *value = expand_env(cmds, str + i);
-            if (value)
-            {
-                printf("%s", value);
-                free(value);
-                // Skip the variable name
-                while (str[i + 1] && str[i + 1] != '\"' && str[i + 1] != '$')
-                    i++;
+	{
+		if (str[i] == '$' && str[i + 1] && str[i + 1] != '\"')
+		{
+            value = expand_env(cmds, str + i);
+			if (value)
+			{
+				printf("%s", value);
+				free(value);
+                while (str[i + 1] && str[i + 1] != '\"' && str[i + 1] != '$')// Skip the variable name
+					i++;
             }
         }
         else
-            printf("%c", str[i]);
+			printf("%c", str[i]);
         i++;
     }
 }
@@ -71,9 +70,11 @@ void print_double_quoted_content(t_commands *cmds, const char *str)
  * @brief Print content between single quotes (literal)
  * @param str String to process
  */
-void print_single_quoted_content(const char *str)
+void	print_single_quoted_content(const char *str)
 {
-    int i = 1;  // Skip first quote
+    int	i;  // Skip first quote
+
+	i = 1;
     while (str[i] && str[i] != '\'')
     {
         printf("%c", str[i]);
@@ -86,19 +87,22 @@ void print_single_quoted_content(const char *str)
  * @param cmds Command structure
  * @param str String to process
  */
-void print_normal_text(t_commands *cmds, const char *str)
+void	print_normal_text(t_commands *cmds, const char *str)
 {
-    int i = 0;
-    while (str[i])
+    int		i;
+    char	*value;
+
+	i = 0;
+	while (str[i])
     {
-        if (str[i] == '$' && str[i + 1] && str[i + 1] != ' ')
-        {
-            char *value = expand_env(cmds, str + i);
+		if (str[i] == '$' && str[i + 1] && str[i + 1] != ' ')
+		{
+            value = expand_env(cmds, str + i);
             if (value)
             {
-                printf("%s", value);
-                free(value);
-                while (str[i + 1] && str[i + 1] != ' ' && str[i + 1] != '$')
+				printf("%s", value);
+				free(value);
+				while (str[i + 1] && str[i + 1] != ' ' && str[i + 1] != '$')
                     i++;
             }
         }
@@ -112,14 +116,14 @@ void print_normal_text(t_commands *cmds, const char *str)
  * @brief Echo command implementation
  * @param cmds Command structure
  */
-void builtin_echo(t_commands *cmds)
+void	builtin_echo(t_commands *cmds)
 {
-    int     i;
-    int     n_flag;
-    char    first_char;
+    int		i;
+    int		n_flag;
+    char	first_char;
     
     i = 1;
-    n_flag = 0;
+    n_flag = 0; //dont know if this is necessary as we are checking for n_flag in the while loop
     if (ft_strcmp(cmds->c[cmds->cc][i], NL_FLAG))
     {
         n_flag = 1;
